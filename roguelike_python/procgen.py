15ebd68c88163e6c8f72c05a5f2a9cadcc28dba9
higorsilveira <higorsilveira.hs@gmail.com>
2021-02-26 10:08:25 -0400

    pergaminhos

--- roguelike_python/procgen.py
+++ roguelike_python/procgen.py
@@ -51,8 +51,13 @@ def place_entities(
         y = random.randint(room.y1 + 1, room.y2 - 1)
 
         if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
-            if random.random() < 0.8:
-                entity_factories.orc.spawn(dungeon, x, y) 
+            item_chance = random.random()
+            if item_chance < 0.7:
+                entity_factories.orc.spawn(dungeon, x, y)
+            elif item_chance < 0.8:
+                entity_factories.fireball_scroll.spawn(dungeon, x, y)
+            elif item_chance < 0.9:
+                entity_factories.confusion_scroll.spawn(dungeon, x, y)
             else:
                 entity_factories.troll.spawn(dungeon, x, y)
 
